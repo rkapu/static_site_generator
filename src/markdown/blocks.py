cc94457a4611109b6f4840f1af44673ad4e69140
rkapu <romankap1798@gmail.com>
2024-09-15 00:21:25 +0300

    Add block types

--- src/markdown/blocks.py
+++ src/markdown/blocks.py
@@ -1,3 +1,18 @@
+import re
+from enum import Enum
+
+BlockType = Enum(
+    "BlockType",
+    [
+        "PARAGRAPH",
+        "HEADING",
+        "CODE",
+        "QUOTE",
+        "UNORDERED_LIST",
+        "ORDERED_LIST"
+    ]
+)
+
 def markdown_to_blocks(markdown):
     return list(
         filter(
@@ -8,3 +23,40 @@ def markdown_to_blocks(markdown):
             )
         )
     )
+
+def block_to_block_type(block):
+    lines = block.split("\n")
+
+    if re.match(r"#{1,6} ", block):
+        return BlockType.HEADING
+
+    if len(lines) > 1 and block.startswith("```") and block.endswith("```"):
+        return BlockType.CODE
+
+    if block.startswith(">"):
+        for line in lines:
+            if not line.startswith(">"):
+                return BlockType.PARAGRAPH
+        return BlockType.QUOTE
+
+    if block.startswith("* "):
+        for line in lines:
+            if not line.startswith("* "):
+                return BlockType.PARAGRAPH
+        return BlockType.UNORDERED_LIST
+
+    if block.startswith("- "):
+        for line in lines:
+            if not line.startswith("- "):
+                return BlockType.PARAGRAPH
+        return BlockType.UNORDERED_LIST
+
+    if block.startswith("1. "):
+        number = 1
+        for line in lines:
+            if not line.startswith(f"{number}. "):
+                return BlockType.PARAGRAPH
+            number += 1
+        return BlockType.ORDERED_LIST
+
+    return BlockType.PARAGRAPH
--- tests/markdown/test_blocks.py
+++ tests/markdown/test_blocks.py
@@ -2,6 +2,7 @@ import unittest
 import textwrap
 
 from src.markdown.blocks import *
+from tests.helpers import assert_all_cases
 
 class TestBlocks(unittest.TestCase):
     def test_markdown_to_blocks(self):
@@ -25,6 +26,63 @@ class TestBlocks(unittest.TestCase):
         ]
 
         self.assertEqual(expected_result, markdown_to_blocks(markdown))
+
+    def test_block_to_block_type(self):
+        test_cases = [
+            # Heading
+            ["# Heading", BlockType.HEADING],
+            ["## Heading", BlockType.HEADING],
+            ["### Heading", BlockType.HEADING],
+            ["#### Heading", BlockType.HEADING],
+            ["##### Heading", BlockType.HEADING],
+            ["###### Heading", BlockType.HEADING],
+            ["#heading", BlockType.PARAGRAPH],
+            ["####### Heading", BlockType.PARAGRAPH],
+            ["heading", BlockType.PARAGRAPH],
+            ["heading#", BlockType.PARAGRAPH],
+
+            # Code
+            ["```ruby\n return 'something'\n```", BlockType.CODE],
+            ["```a code sample```", BlockType.PARAGRAPH],
+            ["`code`", BlockType.PARAGRAPH],
+            ["``code``", BlockType.PARAGRAPH],
+            ["```code", BlockType.PARAGRAPH],
+            ["code```", BlockType.PARAGRAPH],
+            ["```", BlockType.PARAGRAPH],
+            ["``````", BlockType.PARAGRAPH],
+
+            # Quote
+            [">quote", BlockType.QUOTE],
+            ["> quote", BlockType.QUOTE],
+            [">         quote", BlockType.QUOTE],
+            [">> quote", BlockType.QUOTE],
+            [">\n>quote", BlockType.QUOTE],
+            [">\nquote", BlockType.PARAGRAPH],
+            ["quote", BlockType.PARAGRAPH],
+
+            # Unordered list
+            ["* item", BlockType.UNORDERED_LIST],
+            ["- item", BlockType.UNORDERED_LIST],
+            ["* \n* item\n* item", BlockType.UNORDERED_LIST],
+            ["- \n- item\n- item", BlockType.UNORDERED_LIST],
+            ["*item", BlockType.PARAGRAPH],
+            ["-item", BlockType.PARAGRAPH],
+            ["*item*", BlockType.PARAGRAPH],
+            ["* item\n- item", BlockType.PARAGRAPH],
+
+            # Ordered list
+            ["1. item", BlockType.ORDERED_LIST],
+            ["1. item\n2. item\n3. item", BlockType.ORDERED_LIST],
+            ["1item", BlockType.PARAGRAPH],
+            ["1 item", BlockType.PARAGRAPH],
+            ["2. item", BlockType.PARAGRAPH],
+            ["1. item\n3. item\n4. item", BlockType.PARAGRAPH],
+
+            # Paragraph
+            ["*This* is just a paragraph\n#paragraph", BlockType.PARAGRAPH]
+        ]
+
+        assert_all_cases(self, test_cases, block_to_block_type)
 
 if __name__ == "__main__":
     unittest.main()
